960012c1710e67ab33801058ac0a93064ed36c6e
Aneesh Durg <aneeshdurg17@gmail.com>
2020-04-14 14:48:31 -0700

    Cleanup mapbuilder

--- mapbuilder/mapbuilder.js
+++ mapbuilder/mapbuilder.js
@@ -1,37 +1,272 @@
-const TILEWIDTH = 64;
-const TILEHEIGHT = 64;
-const MAPWIDTH = 16;
-const MAPHEIGHT = 16;
-const TILESPERROW = 8;
+class Map {
+    cols = 16
+    rows = 16
+    tsize = 64
+    tiles = null
+    legalTowerTiles = new Set()
+    edgeMap = {}
 
-async function main() {
-    var map = {
-        cols: MAPWIDTH,
-        rows: MAPHEIGHT,
-        tsize: 64,
-        tiles: new Array(MAPWIDTH * MAPHEIGHT),
-        legalTowerTiles: new Set(),
-        edgeMap: {},
-    };
-    map.tiles.fill(0);
-
-    function getTile(map, col, row) {
-        return map.tiles[row * map.cols + col]
+    constructor() {
+        this.tiles = new Array(this.cols * this.rows);
+        this.tiles.fill(0);
     }
 
-    document.getElementById("downloader").onclick = function() {
-        map.legalTowerTiles = [...map.legalTowerTiles];
-        const map_data = JSON.stringify(map);
-        map.legalTowerTiles = new Set(map.legalTowerTiles);
+    getTile(col, row) {
+        return this.tiles[row * this.cols + col]
+    }
 
+    toString() {
+        return JSON.stringify({
+            cols: this.cols,
+            rows: this.rows,
+            tsize: this.tsize,
+            tiles: this.tiles,
+            legalTowerTiles: [...map.legalTowerTiles],
+            edgeMap: this.edgeMap,
+        });
+    }
+}
+
+class Picker {
+    tower_selected = false
+    path_selected = false
+    pathStart = null
+    selected_tile = 0
+
+    constructor(id, img, tileWidth, tileHeight, tilesPerRow) {
+        this.img = img;
+
+        this.tilesPerRow = tilesPerRow;
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
+        this.max_tile = (this.img.width / this.tileWidth) * (this.img.height / this.tileHeight) + 1;
+
+        this.canvas = document.getElementById("imgatlas");
+        this.canvas.width = this.img.width;
+        this.canvas.height = this.img.height + this.tileWidth;
+
+        this.ctx = this.canvas.getContext("2d");
+        this.canvas.onclick = this.onclick.bind(this);
+        this.onclick();
+    }
+
+    onclick(e) {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.drawImage(this.img, 0, 0);
+        if (e != null) {
+            // update selected_tile
+            const rect = this.canvas.getBoundingClientRect();
+            let x = Math.round(this.canvas.width * (e.clientX - rect.left) / rect.width);
+            x = Math.floor(x / this.tileWidth);
+
+            let y = Math.round(this.canvas.height  * (e.clientY - rect.top) / rect.height);
+            y = Math.floor(y / this.tileHeight);
+
+            this.selected_tile = 1 + x + y * this.tilesPerRow;
+        }
+
+        if (this.selected_tile != 0) {
+            const idx = this.selected_tile - 1;
+            this.ctx.beginPath();
+            this.ctx.lineWidth = "4";
+            this.ctx.strokeStyle = "black";
+            let idxX = this.tileWidth * (idx % this.tilesPerRow);
+            let idxY = this.tileHeight * Math.floor(idx / this.tilesPerRow);
+            this.ctx.rect(idxX + 4, idxY + 4, this.tileWidth - 8, this.tileHeight - 8);
+            this.ctx.stroke();
+        }
+
+        this.ctx.font = '64px monospace';
+        this.ctx.fillText('T', 15, this.canvas.height - 15, 64);
+        this.ctx.fillText('P', 15 + 64, this.canvas.height - 15, 64);
+
+        this.tower_selected = false;
+        this.path_selected = false;
+        this.pathStart = null;
+        if (this.selected_tile >= this.max_tile) {
+            if (this.selected_tile == this.max_tile)
+                this.tower_selected = true;
+            else if (this.selected_tile == this.max_tile + 1)
+                this.path_selected = true;
+
+            this.selected_tile = 0;
+        }
+    }
+}
+
+class Painter {
+    constructor(id, picker, map) {
+        this.canvas = document.getElementById(id);
+        this.canvas.width = picker.tileWidth * map.cols;
+        this.canvas.height = picker.tileHeight * map.rows;
+        this.ctx = this.canvas.getContext("2d");
+
+        this.picker = picker;
+        this.map = map;
+        this.canvas.onclick = this.onclick.bind(this, false);
+        const that = this;
+        that.canvas.oncontextmenu = function(e) {
+            e.preventDefault();
+
+            const temp_tile = that.picker.selected_tile;
+            that.picker.selected_tile = 0;
+            that.onclick(true, e);
+            that.picker.selected_tile = temp_tile;
+        }
+        this.onclick(false);
+    }
+
+    drawExistingMap() {
+        for (var c = 0; c < this.map.cols; c++) {
+            for (var r = 0; r < this.map.rows; r++) {
+                var tile = this.map.getTile(c, r);
+                if (tile !== 0) { // 0 => empty tile
+                    const idx = tile - 1;
+                    this.ctx.drawImage(
+                        this.picker.img, // image
+                        (idx % this.picker.tilesPerRow) * this.map.tsize, // source x
+                        Math.floor(idx / this.picker.tilesPerRow) * this.map.tsize, // source y
+                        this.map.tsize, // source width
+                        this.map.tsize, // source height
+                        c * this.map.tsize, // target x
+                        r * this.map.tsize, // target y
+                        this.map.tsize, // target width
+                        this.map.tsize // target height
+                    );
+                }
+            }
+        }
+    }
+
+    idxToXY(idx) {
+        const x = this.picker.tileWidth * (idx % this.map.cols);
+        const y = this.picker.tileHeight * Math.floor(idx / this.map.cols);
+        return [x, y];
+    }
+
+    drawEdgePaths() {
+        Object.keys(this.map.edgeMap).forEach((start) => {
+            const startCoords = this.idxToXY(start);
+            startCoords[0] += this.picker.tileWidth / 2;
+            startCoords[1] += this.picker.tileHeight / 2;
+
+            this.map.edgeMap[start].forEach((end) => {
+                const endCoords = this.idxToXY(end);
+                endCoords[0] += this.picker.tileWidth / 2;
+                endCoords[1] += this.picker.tileHeight / 2;
+
+                this.ctx.lineWidth = "8";
+                const grad = this.ctx.createLinearGradient(startCoords[0], startCoords[1], endCoords[0], endCoords[1]);
+                grad.addColorStop(0, "white");
+                grad.addColorStop(1, "#00FF00");
+
+                this.ctx.strokeStyle = grad;
+                this.ctx.beginPath();
+                this.ctx.moveTo(startCoords[0], startCoords[1]);
+                this.ctx.lineTo(endCoords[0], endCoords[1]);
+                this.ctx.stroke();
+            });
+        });
+
+        // Highlight tile selected for the start of the path
+        if (this.picker.pathStart != null) {
+            this.ctx.beginPath();
+            this.ctx.lineWidth = "4";
+            this.ctx.strokeStyle = "black";
+            const startCoords = this.idxToXY(this.picker.pathStart);
+            this.ctx.rect(startCoords[0] + 4, startCoords[1] + 4, this.picker.tileWidth - 8, this.picker.tileHeight - 8);
+            this.ctx.stroke();
+        }
+    }
+
+    drawMap() {
+        this.drawExistingMap();
+
+        this.map.legalTowerTiles.forEach((idx) => {
+            this.ctx.font = this.picker.tileWidth + 'px monospace';
+            const coords = this.idxToXY(idx);
+            this.ctx.fillText('T', coords[0] + 15, coords[1] + this.picker.tileHeight - 15);
+        });
+
+        this.drawEdgePaths();
+    }
+
+    onclick(erase, e) {
+        this.ctx.fillStyle = "#2b2b2b";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        if (e != null) {
+            // update selected_tile with selected tile
+            const rect = this.canvas.getBoundingClientRect();
+            let x = Math.round(this.canvas.width * (e.clientX - rect.left) / rect.width);
+            let y = Math.round(this.canvas.height  * (e.clientY - rect.top) / rect.height);
+
+            x = Math.floor(x / this.picker.tileWidth);
+            y = Math.floor(y / this.picker.tileHeight);
+
+            let tileID = x + y * this.map.cols;
+
+            if (this.picker.tower_selected) {
+                if (!erase)
+                    this.map.legalTowerTiles.add(tileID);
+                else
+                    this.map.legalTowerTiles.delete(tileID);
+            } else if (this.picker.path_selected) {
+                if (erase) {
+                    if (this.picker.pathStart != null)
+                        this.picker.pathStart = null;
+                    else
+                        delete this.map.edgeMap[tileID];
+                } else {
+                    if (this.picker.pathStart == null) {
+                        this.picker.pathStart = tileID;
+                    } else {
+                        const pathEnd = tileID;
+                        if (!(this.picker.pathStart in this.map.edgeMap))
+                            this.map.edgeMap[this.picker.pathStart] = []
+                        this.map.edgeMap[this.picker.pathStart].push(pathEnd);
+                        this.picker.pathStart = null;
+                    }
+                }
+            } else
+                this.map.tiles[tileID] = this.picker.selected_tile;
+        }
+
+        this.drawMap();
+    }
+}
+
+async function main() {
+    const map = new Map();
+
+    const img = new Image();
+    await new Promise((r) => {
+        img.onload = r;
+        img.src = "../assets/tile_atlas.png";
+    });
+
+    const TILEWIDTH = 64;
+    const TILEHEIGHT = 64;
+    const TILESPERROW = 8;
+    const picker = new Picker("imgatlas", img, TILEWIDTH, TILEHEIGHT, TILESPERROW);
+
+    const painter = new Painter("display", picker, map);
+
+    document.getElementById("downloader").onclick = function() {
         const el = document.createElement('a');
-        el.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(map_data));
+        el.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(map.toString()));
         el.setAttribute('download', 'map.json');
         el.style.display = 'none';
         document.body.appendChild(el);
         el.click();
         document.body.removeChild(el);
     };
+
+    document.getElementById("input").addEventListener("change", async function() {
+        const file = this.files[0];
+        document.getElementById("filecontents").value = await file.text();
+        document.getElementById("filepanel").style.display = "";
+    });
 
     document.getElementById("load").addEventListener("click", async function() {
         const filecontents = document.getElementById("filecontents");
@@ -46,197 +281,6 @@ async function main() {
 
         document.getElementById("filepanel").style.display = "none";
 
-        drawingEvent();
+        painter.onclick();
     });
-
-    document.getElementById("input").addEventListener("change", async function() {
-        const file = this.files[0];
-        document.getElementById("filecontents").value = await file.text();
-        document.getElementById("filepanel").style.display = "";
-    });
-
-    const draw_canvas = document.getElementById("display");
-    draw_canvas.width = TILEWIDTH * MAPWIDTH;
-    draw_canvas.height = TILEHEIGHT * MAPHEIGHT;
-
-    const draw_ctx = draw_canvas.getContext("2d");
-
-    const img = new Image();
-    await new Promise((r) => {
-        img.onload = r;
-        img.src = "../assets/tile_atlas.png";
-    });
-
-    const picker_canvas = document.getElementById("imgatlas");
-    picker_canvas.width = img.width;
-    picker_canvas.height = img.height + TILEHEIGHT;
-    const picker_ctx = picker_canvas.getContext("2d");
-    let tower_selected = false;
-    let path_selected = false;
-    let pathStart = null;
-    let selected_tile = 0;
-    const max_tile = (img.width / TILEWIDTH) * (img.height / TILEHEIGHT) + 1;
-    function pickerEvent(e) {
-        picker_ctx.clearRect(0, 0, picker_canvas.width, picker_canvas.height);
-        picker_ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width, img.height);
-        if (e != null) {
-            // update selected_tile
-            const rect = picker_canvas.getBoundingClientRect();
-            let x = Math.round(picker_canvas.width * (e.clientX - rect.left) / rect.width);
-            x = Math.floor(x / TILEWIDTH);
-
-            let y = Math.round(picker_canvas.height  * (e.clientY - rect.top) / rect.height);
-            y = Math.floor(y / TILEHEIGHT);
-
-            selected_tile = 1 + x + y * TILESPERROW;
-        }
-
-        if (selected_tile != 0) {
-            const idx = selected_tile - 1;
-            picker_ctx.beginPath();
-            picker_ctx.lineWidth = "4";
-            picker_ctx.strokeStyle = "black";
-            let idxX = TILEWIDTH * (idx % TILESPERROW);
-            let idxY = TILEHEIGHT * Math.floor(idx / TILESPERROW);
-            picker_ctx.rect(idxX + 4, idxY + 4, TILEWIDTH - 8, TILEHEIGHT - 8);
-            picker_ctx.stroke();
-        }
-
-        picker_ctx.font = '64px monospace';
-        picker_ctx.fillText('T', 15, picker_canvas.height - 15, 64);
-        picker_ctx.fillText('P', 15 + 64, picker_canvas.height - 15, 64);
-
-        tower_selected = false;
-        path_selected = false;
-        pathStart = null;
-        if (selected_tile >= max_tile) {
-            if (selected_tile == max_tile)
-                tower_selected = true;
-            else if (selected_tile == max_tile + 1)
-                path_selected = true;
-
-            selected_tile = 0;
-        }
-    }
-    picker_canvas.onclick = pickerEvent;
-    pickerEvent();
-
-    function drawMap() {
-        for (var c = 0; c < map.cols; c++) {
-            for (var r = 0; r < map.rows; r++) {
-                var tile = getTile(map, c, r);
-                if (tile !== 0) { // 0 => empty tile
-                    const idx = tile - 1;
-                    draw_ctx.drawImage(
-                        img, // image
-                        (idx % TILESPERROW) * map.tsize, // source x
-                        Math.floor(idx / TILESPERROW) * map.tsize, // source y
-                        map.tsize, // source width
-                        map.tsize, // source height
-                        c * map.tsize, // target x
-                        r * map.tsize, // target y
-                        map.tsize, // target width
-                        map.tsize // target height
-                    );
-                }
-            }
-        }
-
-        function idxToXY(idx) {
-            const x = TILEWIDTH * (idx % MAPWIDTH);
-            const y = TILEHEIGHT * Math.floor(idx / MAPWIDTH);
-            return [x, y];
-        }
-
-        map.legalTowerTiles.forEach((idx) => {
-            draw_ctx.font = TILEWIDTH + 'px monospace';
-            const coords = idxToXY(idx);
-            draw_ctx.fillText('T', coords[0] + 15, coords[1] + TILEHEIGHT - 15);
-        });
-
-        Object.keys(map.edgeMap).forEach((start) => {
-            const startCoords = idxToXY(start);
-            startCoords[0] += TILEWIDTH / 2;
-            startCoords[1] += TILEHEIGHT / 2;
-
-            map.edgeMap[start].forEach((end) => {
-                const endCoords = idxToXY(end);
-                endCoords[0] += TILEWIDTH / 2;
-                endCoords[1] += TILEHEIGHT / 2;
-
-                draw_ctx.lineWidth = "8";
-                const grad = draw_ctx.createLinearGradient(startCoords[0], startCoords[1], endCoords[0], endCoords[1]);
-                grad.addColorStop(0, "white");
-                grad.addColorStop(1, "#00FF00");
-
-                draw_ctx.strokeStyle = grad;
-                draw_ctx.beginPath();
-                draw_ctx.moveTo(startCoords[0], startCoords[1]);
-                draw_ctx.lineTo(endCoords[0], endCoords[1]);
-                draw_ctx.stroke();
-            });
-        });
-
-        if (pathStart != null) {
-            draw_ctx.beginPath();
-            draw_ctx.lineWidth = "4";
-            draw_ctx.strokeStyle = "black";
-            const startCoords = idxToXY(pathStart);
-            draw_ctx.rect(startCoords[0] + 4, startCoords[1] + 4, TILEWIDTH - 8, TILEHEIGHT - 8);
-            draw_ctx.stroke();
-        }
-    }
-
-    function drawingEvent(e, erase) {
-        draw_ctx.fillStyle = "#2b2b2b";
-        draw_ctx.fillRect(0, 0, draw_canvas.width, draw_canvas.height);
-
-        if (e != null) {
-            // update selected_tile with selected tile
-            const rect = draw_canvas.getBoundingClientRect();
-            let x = Math.round(draw_canvas.width * (e.clientX - rect.left) / rect.width);
-            let y = Math.round(draw_canvas.height  * (e.clientY - rect.top) / rect.height);
-
-            x = Math.floor(x / TILEWIDTH);
-            y = Math.floor(y / TILEHEIGHT);
-
-            if (tower_selected) {
-                if (!erase)
-                    map.legalTowerTiles.add(x + y * MAPWIDTH);
-                else
-                    map.legalTowerTiles.delete(x + y * MAPWIDTH);
-            } else if (path_selected) {
-                if (erase) {
-                    if (pathStart != null)
-                        pathStart = null;
-                    else
-                        delete map.edgeMap[x + y * MAPWIDTH];
-                } else {
-                    if (pathStart == null) {
-                        pathStart = x + y * MAPWIDTH;
-                    } else {
-                        const pathEnd = x + y * MAPWIDTH;
-                        if (!(pathStart in map.edgeMap))
-                            map.edgeMap[pathStart] = []
-                        map.edgeMap[pathStart].push(pathEnd);
-                        pathStart = null;
-                    }
-                }
-            } else
-                map.tiles[x + y * MAPWIDTH] = selected_tile;
-        }
-
-        drawMap();
-    }
-    draw_canvas.onclick = drawingEvent;
-    draw_canvas.oncontextmenu = function(e) {
-        const temp_tile = selected_tile;
-        selected_tile = 0;
-        drawingEvent(e, true);
-        selected_tile = temp_tile;
-        e.preventDefault();
-    }
-    drawingEvent();
-
-    // TODO support layers
 }
